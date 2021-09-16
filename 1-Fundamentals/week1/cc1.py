eb6c071bd1c27c4c99386bd6ad109e6f3faa8b70
jycmisc <jyc.misc@outlook.com>
2021-09-15 19:58:21 -0700

    week_one_update

---
+++ 1-Fundamentals/week1/cc1.py
@@ -0,0 +1,17 @@
+# Turn this line into a comment
+
+cities = ['Seattle, "Tacoma', 'Bellevue']
+
+a = 1
+print(type(a))
+
+b = 3.14
+print(type(b))
+
+c = 'True'
+print(type(c))
+
+d = a + b
+print(d)
+
+print(type(d))
